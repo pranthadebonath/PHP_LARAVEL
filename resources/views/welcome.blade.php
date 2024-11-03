3e859c9c568eba684958f0d8dc1c951fff048e9a
Prantha Debonath <pranthadebonath@gmail.com>
2024-11-03 19:41:23 +0600

    Use and learn loop properties like -> first,last,odd,even,index,iteration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,26 +1,43 @@
-<h1>Our first page</h1>
-
-{{5+2}}
-
-<br>
-<br>
-<br>
-
-{{"Hello World"}}
-
-{!!"<h1>Hello World</h1>"!!}
-<br>
-<br>
 @php
-    $users = ["Prantha Debonath","Jyotirmay","Shahid kapoor"];
+    $users = ["Prantha Debonath","Jyotirmay","Shahid kapoor","Nirob Kumar","Dipu kumar","Hridoy Borman"];
+    $dept = "CSE";
 @endphp
 <ul>
 @foreach ($users as $user)
-   <li>{{$user}}</li> 
+   <li>{{$loop->index}}-{{$user}}</li> 
+@endforeach
+
+<br>
+<br>
+@foreach ($users as $user)
+   <li>{{$loop->iteration}}-{{$user}}</li> 
+@endforeach
+
+<br>
+<br>
+@foreach ($users as $user)
+<li>{{$loop->count}}</li>
+@endforeach
+
+<br>
+<br>
+@foreach ($users as $user)
+  @if ($loop->first)
+  <li style="color:red">{{$loop->iteration}}-{{$user}}</li> 
+  @elseif($loop->last)
+  <li style="color:green">{{$loop->iteration}}-{{$user}}</li> 
+  @else
+  <li>{{$loop->iteration}}-{{$user}}</li>
+  @endif
+@endforeach
+
+<br>
+<br>
+@foreach ($users as $user)
+  @if ($loop->even)
+  <li style="color:red">{{$loop->iteration}}-{{$user}}</li> 
+  @elseif($loop->odd)
+  <li style="color:green">{{$loop->iteration}}-{{$user}}</li> 
+  @endif
 @endforeach
 </ul>
-<br>
-<br>
-<a href="{{route('about')}}">About</a>
-<br>
-<a href="/post">Post</a>
